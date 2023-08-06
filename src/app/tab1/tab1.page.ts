ed912899afbb8ddc184e3a6176b4589010dd1bfb
xluis23xx <luis_alexander_23_05_96@hotmail.com>
2023-08-06 13:15:46 -0500

    First commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+
+    let numero = 0;
+    //debugger;
+
+    numero = numero + 10;
+
+    numero = 5;
+
+    console.log("Hola");
+  }
 
 }
